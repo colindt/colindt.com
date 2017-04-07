b486bb479aabb284a9b572f9bc0ddb70f046fd58
colindt <cdt1_2@msn.com>
2017-04-07 04:41:28 -0700

    added a resume variant. Added variant support to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-./wkhtmltopdf --print-media-type --page-size Letter -B 0 -L 0 -R 0 -T 0 index.html - | pdftk - cat 1 output resume.pdf
+if [ -z $1 ]; then
+    INFILE="index.html"
+    OUTFILE="resume.pdf"
+else
+    INFILE=$1
+    OUTFILE=$(basename $1 .html).pdf
+fi
+
+echo $INFILE '->' $OUTFILE
+
+./wkhtmltopdf --print-media-type --page-size Letter -B 0 -L 0 -R 0 -T 0 $INFILE - | pdftk - cat 1 output $OUTFILE
